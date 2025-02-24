c4a1eb19b38c2b06617e24d15b6779fd22174ee8
Bernardo Gomes de Melo <bgm@cin.ufpe.br>
2025-02-24 01:17:56 -0300

    Docstring for PipelineBuilder and Logger, and some log

--- main/pipeline_builder.py
+++ main/pipeline_builder.py
@@ -1,17 +1,39 @@
 import importlib
 from typing import Dict
+from logger import Logger
 
 from sklearn.pipeline import Pipeline
 from decouple import config, UndefinedValueError
 
 
 class PipelineBuilder:
+    """
+    A class to build a scikit-learn pipeline from a configuration file.
+
+    This class dynamically instantiates pipeline steps based on a JSONC configuration
+    file and constructs a scikit-learn Pipeline object.
+    """
+
     def __init__(self, data_loader):
         self.data_loader = data_loader
         self.pipeline_config = None
+        self.logger = Logger()
 
-    @staticmethod
-    def _instantiate_step(step_name: str, step_params: dict) -> tuple:
+    def _instantiate_step(self, step_name: str, step_params: Dict) -> tuple:
+        """
+        Dynamically imports and instantiates a scikit-learn transformer.
+
+        Args:
+            step_name (str): The name of the pipeline step.
+            step_params (Dict): Parameters for the step.
+
+        Returns:
+            tuple: A tuple containing the step name and the instantiated object.
+
+        Note:
+            Parameters matching their default values (e.g., `degree=2` for `PolynomialFeatures`)
+            are not displayed in the logs by default. To address this, we explicitly log the parameters.
+        """
         try:
             sklearn_modules = config(
                 "SKLEARN_MODULES",
@@ -27,8 +49,10 @@ class PipelineBuilder:
                     try:
                         module = importlib.import_module(module_name)
                         cls = getattr(module, class_name)
+                        self.logger.log_info(
+                            f"Instantiating {class_name} with params: {params}"
+                        )
                         return step_name, cls(**params)
-
                     except (ModuleNotFoundError, AttributeError):
                         continue
 
@@ -38,6 +62,13 @@ class PipelineBuilder:
             raise RuntimeError(f"Error while trying to instantiate pipeline step: {e}")
 
     def _instantiate_pipeline(self) -> Pipeline:
+        """
+        Dynamically instantiates the pipeline steps based on the loaded configuration.
+
+        Returns:
+            Pipeline: A scikit-learn Pipeline object.
+        """
+
         try:
             if not self.pipeline_config:
                 raise ValueError(
@@ -58,6 +89,15 @@ class PipelineBuilder:
             raise RuntimeError(f"Error while trying to instantiate pipeline: {e}")
 
     def build(self, configs: Dict) -> Pipeline:
+        """
+        Builds and returns the scikit-learn pipeline.
+
+        Args:
+            configs (Dict): The pipeline configuration.
+
+        Returns:
+            Pipeline: A scikit-learn Pipeline object.
+        """
         try:
             self.pipeline_config = configs
             return self._instantiate_pipeline()
