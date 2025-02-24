665c12faafae439efeec8f5d6e3f8c29a8756c45
Bernardo Gomes de Melo <bgm@cin.ufpe.br>
2025-02-24 00:35:31 -0300

    stream_data function working and requirements.txt first commit

--- main/data_loader.py
+++ main/data_loader.py
@@ -1,7 +1,21 @@
+import pyarrow.parquet as pq
+from typing import Generator
+
 
 class DataLoader:
-    def stream_data(self):
-        pass
+
+    @staticmethod
+    def stream_data(filepath: str, batch_size: int) -> Generator:
+        try:
+            parquet_file = pq.ParquetFile(filepath)
+
+            for batch in parquet_file.iter_batches(batch_size=batch_size):
+                df = batch.to_pandas()
+                df.dropna(inplace=True)
+                yield df
+
+        except Exception as e:
+            raise RuntimeError(f"Error reading Parquet file from {filepath}: {e}")
 
     def load_model(self):
         pass
